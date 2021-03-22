17f119a62aab575ef3c6e850c3cb109eff967795
GermanShtey <gvshteynardt@gmail.com>
2021-03-22 16:11:03 +0300

    project preparation

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="browser">
+        <div className="tabs">
+          <div className="tab">
+            <a>Home</a>
+          </div>
+          <div className="tab">
+            <a>About</a>
+          </div>
+          <div className="tab">
+            <a>Features</a>
+          </div>
+        </div>
+        <div className="viewport">Pages Go Here</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
