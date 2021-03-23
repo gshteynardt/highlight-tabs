a4e17ef7ef64419aaf2ba804f295159a89e64e4a
GermanShtey <gvshteynardt@gmail.com>
2021-03-23 10:47:00 +0300

    added component Tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
+import React, { useState, MouseEvent } from 'react';
+
 import './App.scss';
+import Tab from './components/Tab';
+import { Style } from './types';
+
+const initialValue = {
+  '--left': 0,
+  '--opacity': 0,
+};
 
 function App() {
+  const [highlightStyle, setHighlightStyle] = useState<Style>(initialValue);
+
+  const moveHighlight = (e: MouseEvent<HTMLElement>) => {
+
+    setHighlightStyle({
+      '--left': e.nativeEvent.offsetX - 150,
+    });
+  };
+
+  const hideHighlight = (e: MouseEvent<HTMLElement>) => {
+
+    setHighlightStyle(old => ({
+      ...old,
+      '--left': e.nativeEvent.offsetX - 150,
+      '--opacity': 0,
+    }));
+  };
+
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
+          <Tab
+            onMouseOut={hideHighlight}
+            onMouseMove={moveHighlight}
+            style={highlightStyle}
+          >
+            Home
+          </Tab>
           <div className="tab">
-            <a>Home</a>
           </div>
           <div className="tab">
-            <a>About</a>
-          </div>
-          <div className="tab">
-            <a>Features</a>
           </div>
         </div>
         <div className="viewport">Pages Go Here</div>
